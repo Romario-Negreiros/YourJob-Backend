4a34a2d9d06ef8e37bd853561bd3e4c60964e72b
Romario Negreiros <nromario482@gmail.com>
2022-01-26 11:25:18 -0300

    Created App file with express configs(middlewares and routes) and database initialization

---
+++ src/App.ts
@@ -0,0 +1,36 @@
+import express from 'express'
+import cors from 'cors'
+import db from './database'
+
+import { authRoutes } from './app/routes'
+
+class App {
+  public express: express.Application
+
+  constructor () {
+    this.express = express()
+
+    this.middlewares()
+    this.routes()
+    this.database()
+  }
+
+  private middlewares (): void {
+    this.express.use(express.json())
+    this.express.use(cors())
+  }
+
+  private routes (): void {
+    this.express.use('/auth', authRoutes)
+  }
+
+  private async database (): Promise<void> {
+    try {
+      await db.sync()
+    } catch (err) {
+      console.log(`Connection to postgreSQL failed: ${err.message}`)
+    }
+  }
+}
+
+export default new App().express
