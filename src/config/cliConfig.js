2cb72ff851300b667ee46d2ef5a8506c486b1165
Romario Negreiros <nromario482@gmail.com>
2022-01-26 11:26:31 -0300

    Created dbConfig.ts file for sequelize connection with database - Created cliConfig.js file to sequelize-cli connection with database

---
+++ src/config/cliConfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'rOmArIo18o6fAb',
+  database: 'YourJob',
+  define: {
+    timestamps: true,
+    underscored: true
+  }
+}
---
+++ src/config/dbConfig.ts
@@ -0,0 +1,15 @@
+import { Options } from 'sequelize'
+
+const dbConfig: Options = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'rOmArIo18o6fAb',
+  database: 'YourJob',
+  define: {
+    timestamps: true,
+    underscored: true
+  }
+}
+
+export default dbConfig
