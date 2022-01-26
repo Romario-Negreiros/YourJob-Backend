f568a646e018b8b7c2a02d600bae3c6b7aaaa055
Romario Negreiros <nromario482@gmail.com>
2022-01-26 17:49:13 -0300

    Added passwordResetToken and resetTokenExpiration properties to User model and User.init parameter - Added createdAt and updatedAt readonly properties to User model (timestamps)

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -2,16 +2,23 @@ import { Model, DataTypes } from 'sequelize'
 import db from '../../database'
 
 class User extends Model {
-  public name: string
-  public email: string
-  public password: string
+  public id!: string
+  public name!: string
+  public email!: string
+  public password!: string
+  public passwordResetToken!: string | null
+  public resetTokenExpiration!: Date | null
+  public readonly createdAt!: Date
+  public readonly updatedAt!: Date
 }
 
 User.init(
   {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
-    password: DataTypes.STRING
+    password: DataTypes.STRING,
+    passwordResetToken: DataTypes.STRING,
+    resetTokenExpiration: DataTypes.DATE
   },
   {
     tableName: 'users',
