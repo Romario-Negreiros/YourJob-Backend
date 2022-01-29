7fa6bd9d0a9b7079c163e573e6dcb9f7fee99ef2
Romario Negreiros <nromario482@gmail.com>
2022-01-29 15:24:29 -0300

    Removed multer middleware from /register route

--- src/app/routes/auth.ts
+++ src/app/routes/auth.ts
@@ -5,5 +5,7 @@ const router = express.Router()
 
 router.post('/register', authController.register)
 router.post('/authenticate', authController.authenticate)
+router.post('/forgot_password', authController.forgotPassword)
+router.post('/reset_password/:token', authController.resetPassword)
 
 export default router
