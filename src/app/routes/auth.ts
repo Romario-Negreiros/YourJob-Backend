8a941b0d565b53dcde61934a39c817861471bc36
Romario Negreiros <nromario482@gmail.com>
2022-01-30 13:11:22 -0300

    Created verify email route

--- src/app/routes/auth.ts
+++ src/app/routes/auth.ts
@@ -4,6 +4,7 @@ import { authController } from '../controllers'
 const router = express.Router()
 
 router.post('/register', authController.register)
+router.get('/verify_email/:userID/:token', authController.verifyEmail)
 router.post('/authenticate', authController.authenticate)
 router.post('/forgot_password', authController.forgotPassword)
 router.post('/reset_password/:token', authController.resetPassword)
