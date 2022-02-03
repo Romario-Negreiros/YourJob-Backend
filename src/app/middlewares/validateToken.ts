26c97bb948c0d291fa9de8fb44fe36e510f5acd1
Romario Negreiros <nromario482@gmail.com>
2022-02-03 10:23:28 -0300

    Created validateToken middleware to make the json web token authorization

---
+++ src/app/middlewares/validateToken.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+import authConfig from '../../config/jwt.json'
+
+const validateToken = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'No token provided!' })
+  }
+
+  const parts = authHeader.split(' ')
+
+  if (parts.length !== 2) {
+    return res.status(401).json({ error: 'Token error!' })
+  }
+
+  const [scheme, token] = parts
+
+  if (!/^Bearer$/i.test(scheme)) {
+    return res.status(401).json({ error: 'Token malformatted!' })
+  }
+
+  jwt.verify(token, authConfig.secret, (error, decoded) => {
+    if (error) {
+      return res.status(401).json({ error: 'Invalid token!' })
+    }
+    res.locals = {
+      ...decoded
+    }
+  })
+
+  return next()
+}
+
+export default validateToken
