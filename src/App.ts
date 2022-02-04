5d4085bf23d045b1fe5a9e328d93c0bf9c9ab811
Romario Negreiros <nromario482@gmail.com>
2022-02-04 16:58:18 -0300

    Nothing useful

--- src/App.ts
+++ src/App.ts
@@ -15,6 +15,14 @@ class App {
     this.database()
   }
 
+  private async database (): Promise<void> {
+    try {
+      await db.sync()
+    } catch (err) {
+      console.log(`Connection to postgreSQL failed: ${err.message}`)
+    }
+  }
+
   private middlewares (): void {
     this.express.use(express.json())
     this.express.use(cors())
@@ -24,14 +32,6 @@ class App {
     this.express.use('/auth', authRoutes)
     this.express.use('/companies', companiesRoutes)
     this.express.use('/', mainRoutes)
-  }
-
-  private async database (): Promise<void> {
-    try {
-      await db.sync()
-    } catch (err) {
-      console.log(`Connection to postgreSQL failed: ${err.message}`)
-    }
   }
 }
 
