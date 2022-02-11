9e7db870ee1c26c123d20bbebf8923d8701c6c0f
Romario Negreiros <nromario482@gmail.com>
2022-02-11 14:48:32 -0300

    Added options object in sequelize constructor to connect with heroku postgre

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,6 +3,15 @@ import dbConfig from '../config/db'
 import dotEnv from 'dotenv'
 dotEnv.config()
 
-const db = process.env.DATABASE_URL ? new Sequelize(process.env.DATABASE_URL) : new Sequelize(dbConfig)
+const db = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  })
+  : new Sequelize(dbConfig)
 
 export default db
