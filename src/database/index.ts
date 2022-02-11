0baf5dbdd2b14e0d647bb6222c6b15734febf9e2
Romario Negreiros <nromario482@gmail.com>
2022-02-11 16:48:19 -0300

    Added database url to sequelize connection

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,18 +1,20 @@
 import { Sequelize } from 'sequelize'
-// import dbConfig from '../config/db'
+import dbConfig from '../config/db'
 import dotEnv from 'dotenv'
 dotEnv.config()
 
 const db = new Sequelize(
-  'postgres://bvviytrawfaqns:ab1f04008f1313aff283d82672bb958ded5faeeb56fcde014d1cd8ddbb786e63@ec2-18-215-8-186.compute-1.amazonaws.com:5432/ddgp3a4qehhtg8',
-  {
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
-  }
+  process.env.DATABASE_URL
+    ? (process.env.DATABASE_URL,
+      {
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false
+          }
+        }
+      })
+    : dbConfig
 )
 
 export default db
