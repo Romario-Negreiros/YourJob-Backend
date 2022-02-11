f6868e9c05f41c9d723ef1dbda6cd8000acf969b
Romario Negreiros <nromario482@gmail.com>
2022-02-11 12:16:06 -0300

    Added check to see if database_url var is available, if so, connect to the db through this url, if it is not, use the dbConfig file

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,8 @@
 import { Sequelize } from 'sequelize'
 import dbConfig from '../config/db'
+import dotEnv from 'dotenv'
+dotEnv.config()
 
-const db = new Sequelize(dbConfig)
+const db = process.env.DATABASE_URL ? new Sequelize(process.env.DATABASE_URL) : new Sequelize(dbConfig)
 
 export default db
