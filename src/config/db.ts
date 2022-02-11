be6c35ce08f9dd06131c9b450b6c1c73a03644eb
Romario Negreiros <nromario482@gmail.com>
2022-02-11 12:14:33 -0300

    Removed env vars from db config file

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,14 +1,11 @@
 import { Options } from 'sequelize'
-import dotenv from 'dotenv'
-dotenv.config()
 
 const dbConfig: Options = {
   dialect: 'postgres',
-  port: Number(process.env.DBPORT) || 8080,
-  host: process.env.DBHOST || 'localhost',
-  username: process.env.DBUSER || 'postgres',
-  password: process.env.DBPWD || 'rOmArIo18o6fAb',
-  database: process.env.DBNAME || 'YourJob',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'rOmArIo18o6fAb',
+  database: 'YourJob',
   define: {
     timestamps: true
   }
