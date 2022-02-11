27b3a96093034c142082038016de3a1fde199066
Romario Negreiros <nromario482@gmail.com>
2022-02-11 15:27:57 -0300

    Trying to fix heroku problem

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,17 +1,18 @@
 import { Sequelize } from 'sequelize'
-import dbConfig from '../config/db'
+// import dbConfig from '../config/db'
 import dotEnv from 'dotenv'
 dotEnv.config()
 
-const db = process.env.DATABASE_URL
-  ? new Sequelize(process.env.DATABASE_URL, {
+const db = new Sequelize(
+  'postgres://bvviytrawfaqns:ab1f04008f1313aff283d82672bb958ded5faeeb56fcde014d1cd8ddbb786e63@ec2-18-215-8-186.compute-1.amazonaws.com:5432/ddgp3a4qehhtg8',
+  {
     dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false
       }
     }
-  })
-  : new Sequelize(dbConfig)
+  }
+)
 
 export default db
