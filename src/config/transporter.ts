2a186c25e97ff7bb93c3bc93feffe5c8957751f8
Romario Negreiros <nromario482@gmail.com>
2022-03-11 17:48:41 -0300

    Added host prop to fix timeout error

--- src/config/transporter.ts
+++ src/config/transporter.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const transporterConfigs = {
+  host: 'smtp-mail.outlook.com',
   service: 'hotmail',
   user: 'yourjob.donotreply@outlook.com',
   password: process.env.OUTLOOK_PASSWORD
